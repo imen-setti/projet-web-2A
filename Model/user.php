049ae032e9ac06d54bd2a581a0da6e4d2037c9ea
sarra <Sarra.Sodrati@esprit.tn>
2025-04-09 13:38:23 +0100

    first

---
+++ Model/user.php
@@ -0,0 +1,16 @@
+<?php
+class user {
+    publig string $nom;
+    publig string $prenom;
+    publig string $email;
+    publig string $passwrod;
+    publig string $tel;
+    publig string $sexe;
+    publig string $role;
+
+    $user=new user();
+    public function__construct(){
+        $this
+    }
+}
+?>
