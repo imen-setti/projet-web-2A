766b1f4a10937501774f8fbe124741fa25a122cf
imen setti <imen.setti@esprit.tn>
2025-04-28 13:24:30 +0100

    ajout du 2 eme crud

--- .adb
+++
@@ -1,3 +0,0 @@
-      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
-     1249       1    1249      46736  cons1     197609   Apr 14 /usr/bin/bash
-     1355    1249    1355      84924  cons1     197609 20:19:49 /usr/bin/PS
